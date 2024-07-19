5d6157e168fc29df7c90c20b0be1cded2c67db10
venugopalareddy65 <venugopalareddy65@gmail.com>
2024-07-19 08:14:37 +0530

    Add files via upload

---
+++ day1lab1/Myself.java
@@ -0,0 +1,20 @@
+package day1lab1;
+
+public class Myself {
+	//creating main method
+	public static void main(String[] args)
+	{
+	/*displaying 10 lines
+	about myself*/
+	System.out.println("my name is venugopalareddy");
+	System.out.println("I am studying in avanthi pg college");
+	System.out.println("In the stream of MCA Branch");
+	System.out.println("And it is located in dilsukhnagar");
+	System.out.println("They are reacently contacted");
+	System.out.println("With Anudip foundation");
+	System.out.println("They are giving online training");
+	System.out.println("Advanced Program on Java Full Stack Development using Angular");
+	System.out.println("First day class is what is java");
+	System.out.println("Second day class is how to install java");
+	}
+}
